576ac5b5777da22cd197be5f72d19ae42f522d53
Nikita U <nikitajc@mail.ru>
2022-12-04 11:29:05 +0300

    fixed

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -5,9 +5,9 @@ public class Main {
         Post post = new Post();
         post.birthday = new FormDate();
 
-        post.birthday.day = "13";
-        post.birthday.month = "6";
-        post.birthday.year = "1999";
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
         post.name = "Иван";
         post.passport = "4444 № 4444444";
         post.patronymic = "Иванович";
